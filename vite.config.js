bbf3d17ce12f2aaf2f5bb0963378320cef6c1325
wc-su <theblissout@gmail.com>
2025-07-19 05:06:53 +0800

    打包工具從 Webpack 改成 Vite

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+const week="week2"
+
+export default defineConfig({
+  root: `./${week}/src`,
+  base: './',
+  build: {
+    modulePreload: {
+      polyfill: false, // 不加載 polyfill
+    },
+    rollupOptions: {
+      input: {
+        'product-list': resolve(__dirname, `${week}/src/product-list.html`),
+        product: resolve(__dirname, `${week}/src/product.html`),
+      },
+      // 不使用預設 hash
+      output: {
+        // JS chunk 檔案輸出到 js 資料夾
+        chunkFileNames: 'js/[name].js',
+        entryFileNames: 'js/[name].js',
+        // CSS 輸出到 css 資料夾（由 Vite 處理）
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'css/[name][extname]';
+          }
+          return 'assets/[name][extname]'; // 其他資源（如圖片等）
+        },
+      },
+    },
+    minify: false, // 關閉壓縮
+    outDir: '../dist',      // 可改輸出目錄
+    emptyOutDir: true,
+  },
+})
