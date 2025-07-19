f92068ba1b8d51b3e9218fc3fcea9d0fe0d9f766
wc-su <theblissout@gmail.com>
2025-07-19 13:58:33 +0800

    調整 vite.config 依參數 WEEK 處理指令資料夾

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,51 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
+import fs from 'fs'
 
-const week="week1"
+export default defineConfig(({ mode }) =>{
+  const week = process.env.WEEK
+  const rootDir = `${week}/src`
+  if (!week) {
+    throw new Error(`❌ 請使用 WEEK=weekX vite build 來指定週目錄`);
+  }
 
-export default defineConfig({
-  root: `./${week}/src`,
-  base: './',
-  build: {
-    modulePreload: {
-      polyfill: false, // 不加載 polyfill
-    },
-    rollupOptions: {
-      input: {
-        'story': resolve(__dirname, `${week}/src/story.html`),
-        'pixel-perfect-kata': resolve(__dirname, `${week}/src/pixel-perfect-kata.html`),
+  const htmlFiles = fs
+    .readdirSync(resolve(__dirname, rootDir))
+    .filter(file => file.endsWith('.html'))
+
+  // 自動產生 input 對應 { 'page-name': '/path/to/file.html' }
+  const input = Object.fromEntries(
+    htmlFiles.map(file => [
+      file.replace(/\.html$/, ''), // name: a.html -> a
+      resolve(__dirname, rootDir, file) // path
+    ])
+  )
+
+  return {
+    root: rootDir,
+    base: './',
+    build: {
+      modulePreload: {
+        polyfill: false, // 不加載 polyfill
       },
-      // week2
-      // input: {
-      //   'product-list': resolve(__dirname, `${week}/src/product-list.html`),
-      //   product: resolve(__dirname, `${week}/src/product.html`),
-      // },
-      // 不使用預設 hash
-      output: {
-        // JS chunk 檔案輸出到 js 資料夾
-        chunkFileNames: 'js/[name].js',
-        entryFileNames: 'js/[name].js',
-        // CSS 輸出到 css 資料夾（由 Vite 處理）
-        assetFileNames: ({ name }) => {
-          if (name && name.endsWith('.css')) {
-            return 'css/[name][extname]';
-          }
-          return 'assets/[name][extname]'; // 其他資源（如圖片等）
+      minify: false, // 關閉壓縮
+      outDir: '../dist',      // 可改輸出目錄
+      emptyOutDir: true,
+      rollupOptions: {
+        input,
+        output: {
+          // JS chunk 檔案輸出到 js 資料夾
+          chunkFileNames: 'js/[name].js',
+          entryFileNames: 'js/[name].js',
+          // CSS 輸出到 css 資料夾（由 Vite 處理）
+          assetFileNames: chunkInfo => {
+            if (chunkInfo.name && chunkInfo.name.endsWith('.css')) {
+              return 'css/[name].[ext]';
+            }
+            return 'assets/[name].[ext]'; // 其他資源（如圖片等）
+          },
         },
       },
     },
-    minify: false, // 關閉壓縮
-    outDir: '../dist',      // 可改輸出目錄
-    emptyOutDir: true,
-  },
+  }
 })
