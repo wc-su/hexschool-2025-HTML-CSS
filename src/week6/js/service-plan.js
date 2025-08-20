557c850a68eee32758dd7764841b745dba2cff84
wc-su <89247843+wc-su@users.noreply.github.com>
2025-08-20 17:23:58 +0800

    feat: 使用 Animate.css 和 AOS 製造動態效果 (#68)

--- src/week6/js/service-plan.js
+++ src/week6/js/service-plan.js
@@ -1,5 +1,19 @@
 // Import custom CSS
 import "../scss/service-plan.scss";
 
+import 'animate.css';
+
 // Import utils
 import * as utils from './utils'
+
+const btnsHoverEffects = document.querySelectorAll(".custom-btn-hover");
+btnsHoverEffects.forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    el.classList.add("animate__animated", "animate__pulse", "animate__infinite");
+  });
+
+  el.addEventListener("mouseleave", () => {
+    el.classList.remove("animate__animated", "animate__pulse", "animate__infinite");
+  });
+})
+
